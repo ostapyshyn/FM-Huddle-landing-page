bac68ecaa4429d92161c7e8fd877d979e97e84f8
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-09 21:40:27 +0300

    Add styles for desktop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 import Huddle from './componets/Huddle';
+
 
 function App() {
   return (
     <div className="App">
+      
       <Huddle />
     </div>
   );
--- src/componets/Huddle.jsx
+++ src/componets/Huddle.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import { ReactComponent as Info } from '../assets/svg/illustration-mockups.svg';
+import { ReactComponent as Logo } from '../assets/svg/logo.svg';
 
 import styles from '../assets/huddle.module.scss';
+import { FaFacebookF } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 const Huddle = () => {
   return (
-    <main>
-      <Info />
-      <article>
-        <h1>Build The Community Your Fans Will Love</h1>
-        <p>
-          Huddle re-imagines the way we build communities. You have a voice, but so does your
-          audience. Create connections with your users as you engage in genuine discussion.{' '}
-        </p>
-        <button>Register</button>
-      </article>
-    </main>
+    <>
+      <main>
+        <Logo className={styles.logo} />
+        <div className={styles.info}>
+          <Info />
+          <article>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice, but so does your
+              audience. Create connections with your users as you engage in genuine discussion.{' '}
+            </p>
+            <button>Register</button>
+
+            <div className={styles.icons}>
+              <div className={styles.social_icons}>
+                <FaFacebookF className={styles.social} />
+              </div>
+              <div className={styles.social_icons}>
+                <FaTwitter className={styles.social} />
+              </div>
+              <div className={styles.social_icons}>
+                <FaInstagram className={styles.social} />
+              </div>
+            </div>
+          </article>
+        </div>
+      </main>
+    </>
   );
 };
 
